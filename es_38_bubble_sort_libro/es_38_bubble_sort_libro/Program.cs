6a6d331e856a6a57ba0d65273fc3722e54045513
e.andru.3864 <e.andru.3864@vallauri.edu>
2025-11-28 13:29:16 +0100

    compito per casa

---
+++ es_38_bubble_sort_libro/es_38_bubble_sort_libro/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace es_38_bubble_sort_libro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = { 5, 2, 9, 3, 6, 1, 5, 8, 3, 5, 7 };   
+        }
+    }
+}
